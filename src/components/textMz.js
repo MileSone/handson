065f323eba3a26f33af856c01da4b80f5674b0d3
MW-Mac <mingyao.zhu@oracle.com>
2019-06-26 15:47:45 +0800

    ui

--- src/components/textMz.js
+++ src/components/textMz.js
@@ -1,3 +1,4 @@
+import Smooch from 'smooch'
 import ko from 'knockout'
 import textMzTemplate from '../templates/textMz.html'
 
@@ -5,27 +6,34 @@ class textMzViewModel {
   constructor(params) {
     const self = this
 
-    self.message = params.message
-    self.bot = params.bot
+      self.message = params.message;
+      self.bot = params.bot
       console.log(self.message);
       self.title  = ko.observable("");
       self.body  = ko.observable("");
 
       try {
           var items =  self.message.body.split("@+@");
-          self.title(items[0]);
+          self.title(items[0] + '练习');
           self.body(items[1]);
           console.log(self.title());
       }catch (e) {
       }
 
 
-    self.clickOnList = async function (_data, event) {
-        let title = _data.label;
-        self.bot.appendQuestion(title);
-        self.bot.sendMessage(_data.postback, 'postback')
-    }
+
+      self.clickOnList = async function (_data, event) {
+
+          console.log(_data);
+          await self.bot.appendQuestion(_data.text)
+          //
+          Smooch.triggerPostback(_data._id)
+      }
+
+
   }
+
+
 }
 
 ko.components.register('text-mz', {
